e5994d2421ae110dbf68852de25654abda55e9a6
saiashish90 <saiashish90@gmail.com>
2019-10-26 10:25:49 +0530

    removed simran and aaisha

--- recognize_video.py
+++ recognize_video.py
@@ -2,7 +2,7 @@
 # python recognize_video.py --detector face_detection_model \
 #	--embedding-model openface_nn4.small2.v1.t7 \
 #	--recognizer output/recognizer.pickle \
-#	--le output/le.pickle
+#	--le output/le.pickle sklearn.utils._cython_blas
 
 # import the necessary packages
 from imutils.video import VideoStream
